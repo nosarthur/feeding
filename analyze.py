13cf0029f6cee18010367ddb8eacc5e4fe41ab49
Dong Zhou <zhou.dong@gmail.com>
2018-09-11 11:45:26 -0400

    add quantity plot

--- analyze.py
+++ analyze.py
@@ -17,7 +17,17 @@ df = pd.read_csv(
     keep_default_na=False)
 df.rename(columns={'Volume (oz)': 'Vol'}, inplace=True)
 
-# plot
+# plot daily feeding quantity
+ax1 = plt.subplot(211)
+dates = df.index.date
+by_date = df.groupby(dates).sum()
+# bar plot doesn't work out of box because the x-axis is date and it is measured
+# with respect to 1970
+by_date.Vol.plot(style='bo--')
+plt.ylabel('feed (oz)')
+
+# plot daily feeding pattern
+ax2 = plt.subplot(212, sharex=ax1)
 bm = df.loc[df.BM]
 formula = df.loc[df.BM == False]
 for d, color, text in zip([bm, formula], ['g', 'r'],
@@ -31,12 +41,11 @@ for d, color, text in zip([bm, formula], ['g', 'r'],
 #plt.gca().set_ylabel('Hour')
 plt.ylabel('Hour')
 plt.gcf().autofmt_xdate()
-ax = plt.gca()
-ax.set_xlim([min(dates) - day * 2, max(dates) + day])
+ax2.set_xlim([min(dates) - day * 2, max(dates) + day])
 hours = [datetime.time(i) for i in range(0, 24, 2)]
-ax.set_ylim([hours[0], datetime.time(23, 59, 59)])
+ax2.set_ylim([hours[0], datetime.time(23, 59, 59)])
 plt.yticks(hours)
-ax.grid(True, axis='y', linestyle='--')
-ax.invert_yaxis()
+ax2.grid(True, axis='y', linestyle='--')
+ax2.invert_yaxis()
 plt.legend(loc='upper left')
 plt.savefig('image.png')
