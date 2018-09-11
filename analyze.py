e29eb2a63c4da23b05dc4ded192141d621502324
Dong Zhou <zhou.dong@gmail.com>
2018-09-11 02:59:00 -0400

    distinguish bm

--- analyze.py
+++ analyze.py
@@ -7,26 +7,32 @@ import matplotlib.pyplot as plt
 #import matplotlib.dates as md
 
 
+# load data
 data_type = {'BM': bool}
 df = pd.read_csv('data.csv', dtype=data_type,
         index_col=0, parse_dates=[0],
         skipinitialspace=True, keep_default_na=False)
 df.rename(columns={'Volume (oz)': 'Vol'}, inplace=True)
 
-dates = df.index.date
-times = df.index.time
-vol = df.Vol.values ** 2
-day = datetime.timedelta(days=1)
+# plot
+bm = df.loc[df.BM]
+formula = df.loc[df.BM == False]
+for d, color, text in zip([bm, formula], ['g', 'r'], ['breast milk', 'formula']):
+    dates = d.index.date
+    times = d.index.time
+    vol = df.Vol.values ** 2
+    day = datetime.timedelta(days=1)
+    plt.scatter(dates, times, s=vol, c=color,label=text)
 
-plt.scatter(dates, times, s=vol)
 #plt.gca().set_ylabel('Volumes (oz)')
 plt.ylabel('Volume (oz)')
 plt.gcf().autofmt_xdate()
 ax = plt.gca()
-ax.set_xlim([min(dates)-day, max(dates)+day])
-hours = [datetime.datetime.combine(datetime.date.today(), datetime.time(i)) for i in range(24)]
+ax.set_xlim([min(dates)-day*2, max(dates)+day])
 hours = [datetime.time(i) for i in range(0,24,2)]
+ax.set_ylim([hours[0], datetime.time(23, 59, 59)])
 plt.yticks(hours)
 ax.grid(True, axis='y', linestyle='--')
 ax.invert_yaxis()
+plt.legend(loc='upper left')
 plt.savefig('image.png')
